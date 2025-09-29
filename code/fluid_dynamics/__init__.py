ada18f6f990019c4b262377d9dd5bf4c8b58e291
Justin K. Lietz <156801407+justinlietz93@users.noreply.github.com>
2025-09-29 09:45:31 -0500

    Delete code directory

--- code/fluid_dynamics/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Package marker for Python imports.
-
-This file is intentionally minimal; modules under this directory expose
-public APIs documented in the accompanying scripts and notebooks.
-"""
